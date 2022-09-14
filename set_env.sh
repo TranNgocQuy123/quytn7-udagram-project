4bab37b7fa892bf31d310f08cf08a9853690128b
TranNgocQuy123 <quyxe1998@gmail.com>
2022-09-14 23:00:08 +0700

    start phase 1

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=trangocquytmt
+export POSTGRES_PASSWORD=trangocquytmt
+export POSTGRES_HOST=trangocquytmt.cqtnxc8plxpq.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=trangocquytmt
+export AWS_BUCKET=arn:aws:s3:::my-940917795899-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
