85b7f8f722279f05a1a00ae0f301ad44188dc599
root <root@ip-172-31-94-98.ec2.internal>
2022-09-20 09:08:25 +0000

    update file k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://44.204.150.54:8080/api/v0'
+  apiHost: 'http://a4d309615f2824b1b949b379186b3352-1949419584.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
